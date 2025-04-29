2b104c71e25ced697042c58676a09b9445f91eb6
amz_office <m.a.zhang@outlook.com>
2025-04-29 13:24:09 +0100

    define behaviour rate

--- pixels/constants.py
+++ pixels/constants.py
@@ -1,7 +1,15 @@
 """
 This file contains some constants parameters for the pixels pipeline.
 """
+import numpy as np
+
+SAMPLE_RATE = 2000
+
 freq_bands = {
     "ap":[300, 9000],
     "lfp":[0.5, 300],
 }
+
+BEHAVIOUR_HZ = 25000
+
+np.random.seed(BEHAVIOUR_HZ)
