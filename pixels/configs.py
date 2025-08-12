bf74cc607fe0ee89b175e5e68dc63be61cbf7716
amz_office <m.a.zhang@outlook.com>
2025-08-12 16:52:14 +0100

    use spawn to avoid brokenprocesspool error

--- pixels/configs.py
+++ pixels/configs.py
@@ -17,11 +17,12 @@ logging.basicConfig(
 
 # set si job_kwargs
 job_kwargs = dict(
-    mp_context="fork", # linux
+    #mp_context="fork", # linux, but does not work still on 2025 aug 12
+    mp_context="spawn", # mac & win
     chunk_duration='1s',
     progress_bar=True,
-    n_jobs=4, # less worker
-    max_threads_per_worker=8, # but more thread each worker
+    n_jobs=0.8,
+    max_threads_per_worker=1,
 )
 si.set_global_job_kwargs(**job_kwargs)
 
