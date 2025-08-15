7e9857ade82c28a9517dc9a374674c7805fed0d3
amz_office <m.a.zhang@outlook.com>
2025-08-15 18:18:09 +0100

    add typical oscillation bands

--- pixels/constants.py
+++ pixels/constants.py
@@ -8,6 +8,9 @@ SAMPLE_RATE = 2000 # Hz
 freq_bands = {
     "ap":[300, 9000],
     "lfp":[0.5, 500],
+    "theta":[4, 11], # from Tom
+    "gamma":[30, 80], # from Tom
+    "ripple":[110, 220], # from Tom
 }
 
 BEHAVIOUR_HZ = 25000
