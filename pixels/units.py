12d2eec1d50006992576cc948947a84e5dfc2495
amz_office <m.a.zhang@outlook.com>
2025-09-19 20:09:15 +0100

    allows to add stream_id as key

--- pixels/units.py
+++ pixels/units.py
@@ -1,12 +1,33 @@
-class SelectedUnits(list):
+from typing import Iterable
+
+class SelectedUnits(dict[str, list[int]]):
     name: str
     """
-    This is the return type of Behaviour.select_units, which is a list in every way
-    except that when represented as a string, it can return a name, if a `name`
-    attribute has been set on it. This allows methods that have had `units` passed to be
-    cached to file.
+    A mapping from stream_id to lists of unit IDs.
+    Behaves like a dict in every way, except that when represented as a string,
+    it can return a `name` if set. This allows named instances to be cached to file.
     """
-    def __repr__(self):
+
+    def __init__(self, *args, name: str | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if name is not None:
+            self.name = name
+
+    def __repr__(self) -> str:
         if hasattr(self, "name"):
             return self.name
-        return list.__repr__(self)
+        return dict.__repr__(self)
+
+    # Convenience helpers
+    def add(self, stream_id: str, unit_id: int) -> None:
+        self.setdefault(stream_id, []).append(unit_id)
+
+    def extend(self, stream_id: str, unit_ids: Iterable[int]) -> None:
+        self.setdefault(stream_id, []).extend(unit_ids)
+
+    def flat(self) -> list[int]:
+        # If you sometimes need a flat list of all unit IDs
+        out: list[int] = []
+        for ids in self.values():
+            out.extend(ids)
+        return out
